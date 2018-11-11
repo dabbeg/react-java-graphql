86f3676b8780e70322a2fb702659904c6da184e4
Davíð Guðni <davidh@tempo.io>
2018-11-11 00:09:17 +0000

    Added a spring boot to backend with a hello world endpoint

--- back/src/main/java/com/dabbeg/app/App.java
+++ back/src/main/java/com/dabbeg/app/App.java
@@ -1,13 +1,20 @@
 package com.dabbeg.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class App {
+
+  @RequestMapping("/")
+  String home() {
+    return "Hello World!";
+  }
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(App.class, args);
+  }
+
 }
